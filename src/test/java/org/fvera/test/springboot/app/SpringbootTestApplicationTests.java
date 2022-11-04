5914a2983150ae968eaad27c148565f09cc9a69d
fvera <verafer343@gmail.com>
2022-11-04 17:53:40 -0300

    First commit

---
+++ src/test/java/org/fvera/test/springboot/app/SpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fvera.test.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
