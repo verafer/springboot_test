ffd37c3d43b58ad1e6e153aca396e0bed89decc2
fvera <verafer343@gmail.com>
2022-11-04 18:45:13 -0300

    58. Implementando la clase de servicio (Service)

---
+++ src/main/java/org/fvera/test/springboot/app/services/CuentaServiceImpl.java
@@ -0,0 +1,52 @@
+package org.fvera.test.springboot.app.services;
+
+import org.fvera.test.springboot.app.models.Banco;
+import org.fvera.test.springboot.app.models.Cuenta;
+import org.fvera.test.springboot.app.repositories.BancoRepository;
+import org.fvera.test.springboot.app.repositories.CuentaRepository;
+
+import java.math.BigDecimal;
+
+public class CuentaServiceImpl implements CuentaService{
+
+    private CuentaRepository cuentaRepository;
+    private BancoRepository bancoRepository;
+
+    public CuentaServiceImpl(CuentaRepository cuentaRepository, BancoRepository bancoRepository) {
+        this.cuentaRepository = cuentaRepository;
+        this.bancoRepository = bancoRepository;
+    }
+
+    @Override
+    public Cuenta findById(Long id) {
+        return cuentaRepository.findById(id);
+    }
+
+    @Override
+    public int revisarTotalTransferencias(Long bancoId) {
+        Banco banco = bancoRepository.findById(bancoId);
+        return banco.getTotalTransferencias();
+    }
+
+    @Override
+    public BigDecimal revisarSaldo(Long cuentaId) {
+        Cuenta cuenta = cuentaRepository.findById(cuentaId);
+        return cuenta.getSaldo();
+    }
+
+    @Override
+    public void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto) {
+        Banco banco = bancoRepository.findById(1L);
+        int totalTransferencias = banco.getTotalTransferencias();
+        banco.setTotalTransferencias(++totalTransferencias);
+        bancoRepository.update(banco);
+
+        Cuenta cuentaOrigen = cuentaRepository.findById(numCuentaOrigen);
+        cuentaOrigen.debito(monto);;
+        cuentaRepository.update(cuentaOrigen);
+
+        Cuenta cuentaDestino = cuentaRepository.findById(numCuentaDestino);
+        cuentaDestino.credito(monto);
+        cuentaRepository.update(cuentaDestino);
+    }
+}
