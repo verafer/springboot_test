d0d037e7836599bdd3378a4a6bbe2820148104d2
fvera <verafer343@gmail.com>
2022-11-04 22:18:07 -0300

    61. Escribiendo tests assertThrow para afirmar que la excepción lanzada sea correcta

--- src/main/java/org/fvera/test/springboot/app/services/CuentaServiceImpl.java
+++ src/main/java/org/fvera/test/springboot/app/services/CuentaServiceImpl.java
@@ -37,11 +37,6 @@ public class CuentaServiceImpl implements CuentaService{
     @Override
     public void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto,
                            Long bancoId) {
-        Banco banco = bancoRepository.findById(bancoId);
-        int totalTransferencias = banco.getTotalTransferencias();
-        banco.setTotalTransferencias(++totalTransferencias);
-        bancoRepository.update(banco);
-
         Cuenta cuentaOrigen = cuentaRepository.findById(numCuentaOrigen);
         cuentaOrigen.debito(monto);;
         cuentaRepository.update(cuentaOrigen);
@@ -49,5 +44,10 @@ public class CuentaServiceImpl implements CuentaService{
         Cuenta cuentaDestino = cuentaRepository.findById(numCuentaDestino);
         cuentaDestino.credito(monto);
         cuentaRepository.update(cuentaDestino);
+
+        Banco banco = bancoRepository.findById(bancoId);
+        int totalTransferencias = banco.getTotalTransferencias();
+        banco.setTotalTransferencias(++totalTransferencias);
+        bancoRepository.update(banco);
     }
 }
--- src/test/java/org/fvera/test/springboot/app/SpringbootTestApplicationTests.java
+++ src/test/java/org/fvera/test/springboot/app/SpringbootTestApplicationTests.java
@@ -1,8 +1,10 @@
 package org.fvera.test.springboot.app;
 
+import static org.fvera.test.springboot.app.Datos.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import org.fvera.test.springboot.app.exceptions.DineroInsuficienteException;
 import org.fvera.test.springboot.app.models.Banco;
 import org.fvera.test.springboot.app.models.Cuenta;
 import org.fvera.test.springboot.app.repositories.BancoRepository;
@@ -28,13 +30,16 @@ class SpringbootTestApplicationTests {
 		cuentaRepository = mock(CuentaRepository.class);
 		bancoRepository = mock(BancoRepository.class);
 		service = new CuentaServiceImpl(cuentaRepository, bancoRepository);
+		//Datos.CUENTA_001.setSaldo(new BigDecimal("1000"));
+		//Datos.CUENTA_002.setSaldo(new BigDecimal("2000"));
+		//Datos.BANCO.setTotalTransferencias(0);
 	}
 
 	@Test
 	void contextLoads() {
-		when(cuentaRepository.findById(1L)).thenReturn(Datos.CUENTA_001);
-		when(cuentaRepository.findById(2L)).thenReturn(Datos.CUENTA_002);
-		when(bancoRepository.findById(1L)).thenReturn(Datos.BANCO);
+		when(cuentaRepository.findById(1L)).thenReturn(crearCuenta001());
+		when(cuentaRepository.findById(2L)).thenReturn(crearCuenta002());
+		when(bancoRepository.findById(1L)).thenReturn(crearBanco());
 
 		BigDecimal saldoOrigen = service.revisarSaldo(1L);
 		BigDecimal saldoDestino = service.revisarSaldo(2L);
@@ -57,5 +62,36 @@ class SpringbootTestApplicationTests {
 
 		verify(bancoRepository, times(2)).findById(1L);
 		verify(bancoRepository).update(any(Banco.class));
+	}
+
+	@Test
+	void contextLoads2() {
+		when(cuentaRepository.findById(1L)).thenReturn(crearCuenta001());
+		when(cuentaRepository.findById(2L)).thenReturn(crearCuenta002());
+		when(bancoRepository.findById(1L)).thenReturn(crearBanco());
+
+		BigDecimal saldoOrigen = service.revisarSaldo(1L);
+		BigDecimal saldoDestino = service.revisarSaldo(2L);
+		assertEquals("1000", saldoOrigen.toPlainString());
+		assertEquals("2000", saldoDestino.toPlainString());
+
+		assertThrows(DineroInsuficienteException.class, () -> {
+			service.transferir(1L, 2L, new BigDecimal("1200"), 1L);
+		});
+
+		saldoOrigen = service.revisarSaldo(1L);
+		saldoDestino = service.revisarSaldo(2L);
+		assertEquals("1000", saldoOrigen.toPlainString());
+		assertEquals("2000", saldoDestino.toPlainString());
+
+		int total = service.revisarTotalTransferencias(1L);
+		assertEquals(0, total);
+
+		verify(cuentaRepository, times(3)).findById(1L);
+		verify(cuentaRepository, times(2)).findById(2L);
+		verify(cuentaRepository, never()).update(any(Cuenta.class));
+
+		verify(bancoRepository, times(1)).findById(1L);
+		verify(bancoRepository, never()).update(any(Banco.class));
 	}
 }
