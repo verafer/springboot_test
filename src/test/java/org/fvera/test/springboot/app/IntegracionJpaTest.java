143c7df0dce10e5573553213dd8e2461f7435f76
fvera <verafer343@gmail.com>
2022-11-07 11:57:09 -0300

    71. Escribiendo pruebas de integración con @DataJpaTest

---
+++ src/test/java/org/fvera/test/springboot/app/IntegracionJpaTest.java
@@ -0,0 +1,48 @@
+package org.fvera.test.springboot.app;
+
+import org.fvera.test.springboot.app.models.Cuenta;
+import org.fvera.test.springboot.app.repositories.CuentaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class IntegracionJpaTest {
+    @Autowired
+    CuentaRepository cuentaRepository;
+
+    @Test
+    void testFindById() {
+        Optional<Cuenta> cuenta = cuentaRepository.findById(1L);
+        assertTrue(cuenta.isPresent());
+        assertEquals("Andrés", cuenta.orElseThrow().getPersona());
+    }
+
+    @Test
+    void testFindByPersona() {
+        Optional<Cuenta> cuenta = cuentaRepository.findByPersona("Andrés");
+        assertTrue(cuenta.isPresent());
+        assertEquals("Andrés", cuenta.orElseThrow().getPersona());
+        assertEquals("1000.00", cuenta.orElseThrow().getSaldo().toPlainString());
+    }
+
+    @Test
+    void testFindByPersonaThrowException() {
+        Optional<Cuenta> cuenta = cuentaRepository.findByPersona("Rod");
+        assertThrows(NoSuchElementException.class, cuenta::orElseThrow);
+        assertFalse(cuenta.isPresent());
+    }
+
+    @Test
+    void testFindAll() {
+        List<Cuenta> cuentas = cuentaRepository.findAll();
+        assertFalse(cuentas.isEmpty());
+        assertEquals(2, cuentas.size());
+    }
+}
