797b357a95bf07ebaea2eefcec22eb88d42fbf75
fvera <verafer343@gmail.com>
2022-11-15 14:17:54 -0300

    80. Escribiendo pruebas para el controlador

--- src/test/java/org/fvera/test/springboot/app/controllers/CuentaControllerTest.java
+++ src/test/java/org/fvera/test/springboot/app/controllers/CuentaControllerTest.java
@@ -2,8 +2,12 @@ package org.fvera.test.springboot.app.controllers;
 
 import static org.fvera.test.springboot.app.Datos.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.fvera.test.springboot.app.models.Cuenta;
+import org.fvera.test.springboot.app.models.TransaccionDto;
 import org.fvera.test.springboot.app.services.CuentaService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,9 +16,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @WebMvcTest(CuentaController.class)
 class CuentaControllerTest {
@@ -24,6 +33,13 @@ class CuentaControllerTest {
 
     @MockBean
     private CuentaService cuentaService;
+
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
 
     @Test
     void testDetalle() throws Exception {
@@ -39,5 +55,28 @@ class CuentaControllerTest {
                 .andExpect(jsonPath("$.saldo").value("1000"));
 
         verify(cuentaService).findById(1L);
+    }
+
+    @Test
+    void testTransferir() throws Exception {
+
+        // Given
+        TransaccionDto dto = new TransaccionDto();
+        dto.setCuentaOrigenId(1L);
+        dto.setCuentaDestinoId(2L);
+        dto.setMonto(new BigDecimal("100"));
+        dto.setBancoId(1L);
+
+        // When
+        mvc.perform(post("/api/cuentas/transferir")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dto)))
+
+        // Then
+        .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.date").value(LocalDate.now().toString()))
+                .andExpect(jsonPath("$.mensaje").value("Transferencia realizada con éxito!"))
+                .andExpect(jsonPath("$.transaccion.cuentaOrigenId").value(dto.getCuentaOrigenId()));
     }
 }
